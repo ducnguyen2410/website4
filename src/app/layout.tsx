35b40ed51662920fba653cc0faee607c9f8dc05f
nguyen2410 <ducnguyenbernardine@gmail.com>
2024-06-29 16:16:47 +0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "$WITTY",
-  description: "Cutest dog",
+  title: "$CHARIZERD",
+  description: "Dragon",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import dogLogo from "./../../public/dogLogo.png";
 import pumpfund from "../../public/EmT4yP6S_400x400.jpg";
 import telegram from "../../public/teleIcon.svg";
 import twitter from "../../public/twitterIcon.svg";
-import website2_logo from "../../public/website2_logo.jpg";
-import website2_banner from "../../public/website2_banner.jpg";
-import m1 from "../../public/website2_1.jpg"
-import m2 from "../../public/website2_2.jpg"
+import website2_logo from "../../public/website3_logo.jpg";
+import website2_banner from "../../public/website3_banner.jpg";
+import m1 from "../../public/website3_1.jpg"
+// import m2 from "../../public/website2_2.jpg"
 
 export default function Home() {
   return (
     <div className="w-full h-screen overflow-auto flex flex-col items-center">
       <div className="mt-20 items-center gap-10 flex flex-col">
-        <span className="text-[3rem] text-white">$DIGLETT</span>
+        <span className="text-[3rem] text-white">$CHARIZERD</span>
         <Image
           src={website2_banner}
           alt=""
@@ -22,16 +22,15 @@ export default function Home() {
       </div>
       <div className="mt-20 p-6 border-solid border-[3px] border-white rounded-[10px] w-[60%] flex justify-center">
         <span className="text-white text-[2rem]">
-          CA: 2GAT83b7QpH2cqWYuobLAAfqMD6Am8hmCH5hukJcpump
+          CA: Not available
         </span>
       </div>
       <div className="flex flex-col gap-5 mt-20 items-center">
         <Image src={website2_logo} alt="" width={300} />
-        <span className="font-[600] text-white">Most memeable pokemon</span>
+        <span className="font-[600] text-white">Most previous pokemon</span>
       </div>
       <div className="flex flex-row mt-20 gap-10">
         <Image  src={m1} alt="" width={400}/>
-        <Image  src={m2} alt="" width={400}/>
       </div>
       <div className="flex flex-col mt-20 w-full items-center">
         <span className="text-white font-bold text-[2rem]">
@@ -39,7 +38,7 @@ export default function Home() {
         </span>
         <div className="flex flex-row gap-20 mt-10 p-10 border-white border-[3px] rounded-full border-solid w-[60%] justify-center">
           <a
-            href="https://t.me/diglettpokemon"
+            href="https://t.me/charizerdsol"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
@@ -51,7 +50,7 @@ export default function Home() {
             />
           </a>
           <a
-            href="https://x.com/diglettsol"
+            href="https://x.com/charizerdsolana"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
@@ -63,7 +62,7 @@ export default function Home() {
             />
           </a>
           <a
-            href="https://pump.fun/2GAT83b7QpH2cqWYuobLAAfqMD6Am8hmCH5hukJcpump"
+            href="https://t.me/charizerdsol"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
