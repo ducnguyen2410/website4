51a238842d11ff2ea5d67028efa4979ef4355203
nguyen2410 <ducnguyenbernardine@gmail.com>
2024-06-29 15:04:30 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,42 @@ import dogLogo from "./../../public/dogLogo.png";
 import pumpfund from "../../public/EmT4yP6S_400x400.jpg";
 import telegram from "../../public/teleIcon.svg";
 import twitter from "../../public/twitterIcon.svg";
+import website2_logo from "../../public/website2_logo.jpg";
+import website2_banner from "../../public/website2_banner.jpg";
+import m1 from "../../public/website2_1.jpg"
+import m2 from "../../public/website2_2.jpg"
 
 export default function Home() {
   return (
     <div className="w-full h-screen overflow-auto flex flex-col items-center">
       <div className="mt-20 items-center gap-10 flex flex-col">
-        <span className="text-[3rem] text-white">$WITTY $WITTY</span>
+        <span className="text-[3rem] text-white">$DIGLETT</span>
         <Image
-          src={banner}
+          src={website2_banner}
           alt=""
           className="border-[3px] border-solid border-white rounded-[10px]"
         />
       </div>
       <div className="mt-20 p-6 border-solid border-[3px] border-white rounded-[10px] w-[60%] flex justify-center">
-        <span className="text-white text-[2rem]">CA: B8HjnnJVh6Wn8z1eexJg9ZQFNn2P12na8eqQ8V6spump</span>
+        <span className="text-white text-[2rem]">
+          CA: Not Available
+        </span>
       </div>
       <div className="flex flex-col gap-5 mt-20 items-center">
-        <Image src={dogLogo} alt="" width={300}/>
-        <span className="font-[600] text-white">The tiniest dog on Solana</span>
+        <Image src={website2_logo} alt="" width={300} />
+        <span className="font-[600] text-white">Most memeable pokemon</span>
+      </div>
+      <div className="flex flex-row mt-20 gap-10">
+        <Image  src={m1} alt="" width={400}/>
+        <Image  src={m2} alt="" width={400}/>
       </div>
       <div className="flex flex-col mt-20 w-full items-center">
-        <span className="text-white font-bold text-[2rem]">Buy this puppy at</span>
+        <span className="text-white font-bold text-[2rem]">
+          Buy this through
+        </span>
         <div className="flex flex-row gap-20 mt-10 p-10 border-white border-[3px] rounded-full border-solid w-[60%] justify-center">
           <a
-            href="https://t.me/wittysolana"
+            href="https://t.me/diglettpokemon"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
@@ -39,7 +51,7 @@ export default function Home() {
             />
           </a>
           <a
-            href="https://x.com/wittysolana"
+            href="https://x.com/diglettsol"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
@@ -51,7 +63,7 @@ export default function Home() {
             />
           </a>
           <a
-            href="https://pump.fun/B8HjnnJVh6Wn8z1eexJg9ZQFNn2P12na8eqQ8V6spump"
+            href="https://t.me/diglettpokemon"
             className="rounded-full border-[3px] border-solid border-black"
             target="_blank"
           >
